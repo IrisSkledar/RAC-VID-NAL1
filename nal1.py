721fa31fd7af7e41cd5302dde8386d1fbfd1f2b5
IrisSkledar <iris.skledar@student.um.si>
2025-03-23 18:00:53 +0100

    Dodatna razlaga v komentarjih, popravki

--- nal1.py
+++ nal1.py
@@ -1,13 +1,10 @@
-
 import cv2 as cv
 import numpy as np
 import time
 
-
 def zmanjsaj_sliko(slika, sirina, visina):
     '''Zmanjšaj sliko na velikost sirina x visina.'''
     return cv.resize(slika, (sirina, visina), interpolation=cv.INTER_AREA) #INTER_AREA metoda za boljše zmanjšanje slik z detajli
-
 
 def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) -> list:
     '''Sprehodi se skozi sliko v velikosti škatle (sirina_skatle x visina_skatle) in izračunaj število pikslov kože v vsaki škatli.
@@ -31,15 +28,13 @@ def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) ->
 
     return rezultat
 
-
 def prestej_piklse_z_barvo_koze(slika, barva_koze) -> int:
     '''Prestej število pikslov z barvo kože v škatli.'''
     spodnja_meja, zgornja_meja = barva_koze
-    maska = cv.inRange(slika, spodnja_meja, zgornja_meja)  # Ustvarimo masko za barvo kože
-    return cv.countNonZero(maska)  # Štejemo število pikslov, ki ustrezajo barvi kože
+    maska = cv.inRange(slika, spodnja_meja, zgornja_meja) # Ustvarimo masko za barvo kože
+    return cv.countNonZero(maska) # Štejemo število pikslov, ki ustrezajo barvi kože
 
-
-def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj) -> tuple:
+def doloci_barvo_koze(slika,levo_zgoraj,desno_spodaj) -> tuple:
     '''Ta funkcija se kliče zgolj 1x na prvi sliki iz kamere.
     Vrne barvo kože v območju ki ga definira oklepajoča škatla (levo_zgoraj, desno_spodaj).
       Način izračuna je prepuščen vaši domišljiji.'''
@@ -67,8 +62,8 @@ def narisi_skatle(slika, rezultat, sirina_skatle, visina_skatle, prag=300):
                 zgoraj = y * visina_skatle
                 levo = x * sirina_skatle
                 cv.rectangle(slika, (levo, zgoraj),
-                             (levo + sirina_skatle, zgoraj + visina_skatle),
-                             (0, 255, 0), 2)
+                                    (levo+sirina_skatle, zgoraj+visina_skatle),
+                                    (0, 255, 0), 2)
 
 
 def izracunaj_fps(start_time, frame_count):
@@ -79,33 +74,34 @@ def izracunaj_fps(start_time, frame_count):
     return fps
 
 def narisi_modri_okvir(slika, levo_zgoraj, desno_spodaj):
-    pass
+    '''Nariši modri okvir okoli določenega območja.'''
+    cv.rectangle(slika, levo_zgoraj, desno_spodaj, (255, 0, 0), 2)
 
 if __name__ == '__main__':
-    # Pripravi kamero
-    kamera = cv.VideoCapture(0)
-    start_time = time.time()
-    frame_count = 0
-    barva_koze = None
-    # Izračunamo barvo kože na prvi sliki
 
+    #Pripravi kamero
+    kamera = cv.VideoCapture(0)
+    start_time = time.time()  # Začetni čas za FPS
+    frame_count = 0  # Število sličic
+    barva_koze = None  # Inicializiramo barvo kože kot None, dokler ne določimo
+    # Izračunamo barvo kože na prvi sliki
+    # Določi meje za barvo kože (na začetku nastavite območje za določitev barve kože)
     sirina_slike = 340
     visina_slike = 220
-
+    # Dimenzije škatle (za izračun povpr. barve 1 sliki)
     sirina_skatle = 100
     visina_skatle = 100
-
+    # Izračunamo levo zgoraj in desno spodaj, da bo škatla na sredini
     levo_zgoraj = ((sirina_slike - sirina_skatle) // 2, (visina_slike - visina_skatle) // 2)
     desno_spodaj = (levo_zgoraj[0] + sirina_skatle, levo_zgoraj[1] + visina_skatle)
     while True:
-    # Zajami prvo sliko iz kamere
+    #Zajami prvo sliko iz kamere
         ret, slika = kamera.read()
         if not ret:
             break
 
         # Zmanjšaj sliko na ustrezno velikost
         slika = zmanjsaj_sliko(slika, sirina_slike, visina_slike)
-        # Zajemaj slike iz kamere in jih obdeluj
 
         if barva_koze is None:  # Samo prvič, ko določimo barvo kože
             # Nariši modri okvir okoli območja za določitev barve kože
@@ -117,7 +113,7 @@ if __name__ == '__main__':
                 print("Barva kože določena:", barva_koze)
 
         if barva_koze is not None:  # Ko imamo določeno barvo kože
-            # Obdelava slike s škatlami in preštevanje pikslov kože
+        # Obdelava slike s škatlami in preštevanje pikslov kože
             rezultat = obdelaj_sliko_s_skatlami(slika, 10, 10, barva_koze)
 
             # Nariši škatle z dovolj pikslov kože
@@ -140,11 +136,20 @@ if __name__ == '__main__':
     # Zapri kamero in okna
     kamera.release()
     cv.destroyAllWindows()
+    #^^^^Označi območja (škatle), kjer se nahaja obraz (kako je prepuščeno vaši domišljiji)
 
-    # Označi območja (škatle), kjer se nahaja obraz (kako je prepuščeno vaši domišljiji)
-    # Vprašanje 1: Kako iz števila pikslov iz vsake škatle določiti celotno območje obraza (Floodfill)?
-    # Vprašanje 2: Kako prešteti število ljudi?
+        #Vprašanje 1: Kako iz števila pikslov iz vsake škatle določiti celotno območje obraza (Floodfill)?
+    #funkcija obdelaj_sliko_s_skatlami, kjer se slika razdeli na neprekrivajoče pravokotnike (škatle) določene velikosti (npr. 10x10 pikslov).
+    # Za vsako škatlo se izračuna, koliko pikslov v njej ustreza barvi kože. Vse to se shrani v 2D seznam (rezultat), kjer vsako polje predstavlja število pikslov kože v eni škatli.
+    # Nato funkcija narisi_skatle nariše zelen pravokotnik okoli tistih škatel,
+    # kjer število kožnih pikslov presega določen prag.
 
-    # Kako velikost prebirne škatle vpliva na hitrost algoritma in točnost detekcije? Poigrajte se s parametroma velikost_skatle
-    # in ne pozabite, da ni nujno da je škatla kvadratna.
-    
+        #Vprašanje 2: Kako prešteti število ljudi?
+    #Število ljudi bi ocenila kot število ločenih skupin škatel z dovolj kožnimi piksli.
+    # Vsako skupino dobim z FloodFill metodo.
+    # Vsaka skupina predstavlja en obraz oz ena oseba.
+
+        #Kako velikost prebirne škatle vpliva na hitrost algoritma in točnost detekcije? Poigrajte se s parametroma velikost_skatle
+    # Manjša škatla = večja natančnost, počasnejši algoritem; večja škatla = hitrejši alg., slabša natančnost
+
+        #in ne pozabite, da ni nujno da je škatla kvadratna.
